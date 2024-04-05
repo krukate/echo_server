7afd377e6dba5a0658d447c8cd84d3e1d36dcccc
krukate <k.krupinova@gmail.com>
2024-04-05 16:41:23 +0300

    second commit

--- client.py
+++ client.py
@@ -1,10 +1,24 @@
 import socket
 
+host = input("Введите имя хоста (по умолчанию 127.0.0.1): ") or '127.0.0.1'
+port = int(input("Введите номер порта (по умолчанию 9090): ") or 9090)
+
 sock = socket.socket()
-sock.connect(('127.0.0.1', 9090))
+sock.connect((host, port))
 print("Подключение к серверу")
 
+first_connection = True
+
 while True:
+    if first_connection:
+        response = sock.recv(1024).decode()
+        print(response)
+        if "Пожалуйста, введите свое имя" in response:
+            name = input("Введите ваше имя: ")
+            sock.send(name.encode())
+            first_connection = False
+        else:
+            first_connection = False
 
     msg = input("Введите сообщение для отправки: ")
 
--- server.py
+++ server.py
@@ -1,26 +1,58 @@
 import socket
+import logging
+
+logging.basicConfig(filename='server_log.txt', level=logging.INFO, format='%(asctime)s - %(message)s')
+
+# Словарь для хранения клиентов (IP-адрес : имя)
+clients = {}
+
+host = input("Введите имя хоста (по умолчанию 127.0.0.1): ") or '127.0.0.1'
+port = int(input("Введите номер порта (по умолчанию 9090): ") or 9090)
 
 sock = socket.socket()
-sock.bind(('127.0.0.1', 9090))
-print("Запуск сервера")
+while True:
+    try:
+        sock.bind((host, port))
+        break
+    except OSError:
+        logging.warning(f"Порт {port} занят, пытаюсь найти доступный порт")
+        port += 1
+
+print(f"Сервер запущен на порту: {port}")
+logging.info(f"Запуск сервера на порту: {port}")
+
 sock.listen(0)
-print("Начало прослушивания")
+logging.info("Начало прослушивания")
 
 while True:
     conn, addr = sock.accept()
-    print(f"Подключен клиент: {addr}")
+    logging.info(f"Подключен клиент: {addr}")
+
+    client_ip = addr[0]
+
+    if client_ip in clients:
+        conn.send(f"Привет, {clients[client_ip]}! Если хотите выйти, введите 'exit'".encode())
+    else:
+        conn.send("Добро пожаловать! Пожалуйста, введите свое имя: ".encode())
+        data = conn.recv(1024)
+        client_name = data.decode()
+        clients[client_ip] = client_name
+        logging.info(f"Новый клиент добавлен: {client_name}")
 
     while True:
         data = conn.recv(1024)
         msg = data.decode()
-        if msg == "exit":
-            print("Клиент запросил отключение")
-            conn.close()
-            break
+        if msg:
+            if msg == "exit":
+                logging.info("Клиент запросил отключение")
+                conn.close()
+                break
 
-        print(f"Данные от клиента получены: {msg}")
-        conn.send(data)
-        print(f"Данные отправлены клиенту: {msg}")
+            logging.info(f"Данные от клиента получены: {msg}")
+            conn.send(data)
+            logging.info(f"Данные отправлены клиенту: {msg}")
 
-#print("Остановка сервера")
-#sock.close()
+            # Дополнительно сохраняем обновленный список клиентов в файл
+    with open('clients.txt', 'w') as file:
+        for ip, name in clients.items():
+            file.write(f"{ip}:{name}\n")
